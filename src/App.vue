3c22c38392871eb7d02a9972bdee96961cf615e0
Matthew Galligan <matthew.galligan@navy.mil>
2020-09-30 16:55:24 -0400

    Added stonks page, vuex

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Crypto Prices</router-link> |
+      <router-link to="/news">News</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
